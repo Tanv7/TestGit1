5e3c01e81f41138cc50a3ae20b16f3cded7cd55e
John Doe <marjaivanna79@gmail.com>
2024-04-01 12:29:30 +0300

    создал класс Точка

--- src/main/java/com/example/newproject/HelloApplication.java
+++ src/main/java/com/example/newproject/HelloApplication.java
@@ -7,28 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }public class Main{
-
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-    class Point{
-        double x,y;
-        public Point (double x, double y) {
-            this.x = x;
-            this.y = y;
+public class Main{
+    public static void main( String [] args) {
+        Point p= new Point (6.8);
+         Rectangle rect= newRectangle (10,15);
+         System.out.println ("площадь прямоугольника"+ rect.getArea());
         }
-
         }
+        class Point{
+    double
     }
 }
